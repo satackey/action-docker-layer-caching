9da085eafe8c88d32d9030ac77be727eb8c85b10
satackey <21271711+satackey@users.noreply.github.com>
2020-06-14 19:22:57 +0900

    silent find

--- src/LayerCache.ts
+++ src/LayerCache.ts
@@ -77,7 +77,7 @@ class LayerCache {
   }
 
   private async moveLayerTarsInDir(fromDir: string, toDir: string) {
-    const layerTars = (await exec.exec(`find . -name layer.tar`, [], { cwd: fromDir })).stdoutStr.split(`\n`).filter(tar => tar !== '')
+    const layerTars = (await exec.exec(`find . -name layer.tar`, [], { cwd: fromDir, silent: true })).stdoutStr.split(`\n`).filter(tar => tar !== '')
     const moveLayer = async (layer: string) => {
       const from = path.resolve(`${fromDir}/${layer}`)
       const to = path.resolve(`${toDir}/${layer}`)
