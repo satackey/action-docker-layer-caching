b9a1fb7dc3892dd7fce02af22baae403effd6559
satackey <21271711+satackey@users.noreply.github.com>
2020-12-06 12:49:41 +0900

    Prevent the save from failing when the restore fails

--- main.ts
+++ main.ts
@@ -10,6 +10,7 @@ const main = async () => {
   const imageDetector = new ImageDetector()
 
   const alreadyExistingImages = await imageDetector.getExistingImages()
+  core.saveState(`already-existing-images`, JSON.stringify(alreadyExistingImages))
 
   const layerCache = new LayerCache([])
   layerCache.concurrency = parseInt(core.getInput(`concurrency`, { required: true }), 10)
@@ -17,11 +18,13 @@ const main = async () => {
   await layerCache.cleanUp()
 
   core.saveState(`restored-key`, JSON.stringify(restoredKey !== undefined ? restoredKey : ''))
-  core.saveState(`already-existing-images`, JSON.stringify(alreadyExistingImages))
   core.saveState(`restored-images`, JSON.stringify(await imageDetector.getImagesShouldSave(alreadyExistingImages)))
 }
 
 main().catch(e => {
   console.error(e)
   core.setFailed(e)
+
+  core.saveState(`restored-key`, JSON.stringify(``))
+  core.saveState(`restored-images`, JSON.stringify([]))
 })
