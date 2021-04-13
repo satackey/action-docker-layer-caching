2ca743aeedd4a0773d3a2889976f9042544a8783
dabreadman <tany3@tcd.ie>
2021-04-14 00:21:04 +0100

    Add Podman option support

--- main.ts
+++ main.ts
@@ -1,30 +1,57 @@
-import * as core from '@actions/core'
-import * as exec from 'actions-exec-listener'
-import { LayerCache } from './src/LayerCache'
-import {  ImageDetector } from './src/ImageDetector'
+import * as core from "@actions/core";
+import { LayerCache } from "./src/LayerCache";
+import { ImageDetector } from "./src/ImageDetector";
 
 const main = async () => {
-  const primaryKey = core.getInput(`key`, { required: true })
-  const restoreKeys = core.getInput(`restore-keys`, { required: false }).split(`\n`).filter(key => key !== ``)
+  const primaryKey = core.getInput(`key`, { required: true });
+  const restoreKeys = core
+    .getInput(`restore-keys`, { required: false })
+    .split(`\n`)
+    .filter((key) => key !== ``);
 
-  const imageDetector = new ImageDetector()
+  const imageDetector = new ImageDetector();
 
-  const alreadyExistingImages = await imageDetector.getExistingImages()
-  core.saveState(`already-existing-images`, JSON.stringify(alreadyExistingImages))
+  const container = core.getInput(`container`).toLowerCase();
+  if (container !== "docker" && container !== "podman") {
+    throw new Error("Wrong container name: " + container);
+  }
 
-  const layerCache = new LayerCache([])
-  layerCache.concurrency = parseInt(core.getInput(`concurrency`, { required: true }), 10)
-  const restoredKey = await layerCache.restore(primaryKey, restoreKeys)
-  await layerCache.cleanUp()
+  const alreadyExistingImages = await imageDetector.getExistingImages(
+    container
+  );
+  core.saveState(
+    `already-existing-images`,
+    JSON.stringify(alreadyExistingImages)
+  );
 
-  core.saveState(`restored-key`, JSON.stringify(restoredKey !== undefined ? restoredKey : ''))
-  core.saveState(`restored-images`, JSON.stringify(await imageDetector.getImagesShouldSave(alreadyExistingImages)))
-}
+  const layerCache = new LayerCache([]);
+  layerCache.concurrency = parseInt(
+    core.getInput(`concurrency`, { required: true }),
+    10
+  );
+  const restoredKey = await layerCache.restore(
+    primaryKey,
+    container,
+    restoreKeys
+  );
+  await layerCache.cleanUp();
 
-main().catch(e => {
-  console.error(e)
-  core.setFailed(e)
+  core.saveState(
+    `restored-key`,
+    JSON.stringify(restoredKey !== undefined ? restoredKey : "")
+  );
+  core.saveState(
+    `restored-images`,
+    JSON.stringify(
+      await imageDetector.getImagesShouldSave(alreadyExistingImages, container)
+    )
+  );
+};
 
-  core.saveState(`restored-key`, JSON.stringify(``))
-  core.saveState(`restored-images`, JSON.stringify([]))
-})
+main().catch((e) => {
+  console.error(e);
+  core.setFailed(e);
+
+  core.saveState(`restored-key`, JSON.stringify(``));
+  core.saveState(`restored-images`, JSON.stringify([]));
+});
--- src/ImageDetector.ts
+++ src/ImageDetector.ts
@@ -1,20 +1,40 @@
-import * as exec from 'actions-exec-listener'
-import * as core from '@actions/core'
+import * as exec from "actions-exec-listener";
+import * as core from "@actions/core";
 
 export class ImageDetector {
-  async getExistingImages(): Promise<string[]> {
-    const existingSet = new Set<string>([])
-    const ids = (await exec.exec(`docker image ls -q`, [], { silent: true, listeners: { stderr: console.warn }})).stdoutStr.split(`\n`).filter(id => id !== ``)
-    const repotags = (await exec.exec(`docker`, `image ls --format {{.Repository}}:{{.Tag}} --filter dangling=false`.split(' '), { silent: true, listeners: { stderr: console.warn }})).stdoutStr.split(`\n`).filter(id => id !== ``);
+  async getExistingImages(container: "docker" | "podman"): Promise<string[]> {
+    const existingSet = new Set<string>([]);
+    const ids = (
+      await exec.exec(`${container} image ls -q`, [], {
+        silent: true,
+        listeners: { stderr: console.warn },
+      })
+    ).stdoutStr
+      .split(`\n`)
+      .filter((id) => id !== ``);
+    const repotags = (
+      await exec.exec(
+        `${container}`,
+        `image ls --format {{.Repository}}:{{.Tag}} --filter dangling=false`.split(
+          " "
+        ),
+        { silent: true, listeners: { stderr: console.warn } }
+      )
+    ).stdoutStr
+      .split(`\n`)
+      .filter((id) => id !== ``);
     core.debug(JSON.stringify({ log: "getExistingImages", ids, repotags }));
-    ([...ids, ...repotags]).forEach(image => existingSet.add(image))
-    core.debug(JSON.stringify({ existingSet }))
-    return Array.from(existingSet)
+    [...ids, ...repotags].forEach((image) => existingSet.add(image));
+    core.debug(JSON.stringify({ existingSet }));
+    return Array.from(existingSet);
   }
 
-  async getImagesShouldSave(alreadRegisteredImages: string[]): Promise<string[]> {
-    const resultSet = new Set(await this.getExistingImages())
-    alreadRegisteredImages.forEach(image => resultSet.delete(image))
-    return Array.from(resultSet)
+  async getImagesShouldSave(
+    alreadRegisteredImages: string[],
+    container: "docker" | "podman"
+  ): Promise<string[]> {
+    const resultSet = new Set(await this.getExistingImages(container));
+    alreadRegisteredImages.forEach((image) => resultSet.delete(image));
+    return Array.from(resultSet);
   }
 }
